5ab0824e3a2c46527b58bec49e67834e85808f0a
Matthew Gasparetti <106164674+b3nj4m1no@users.noreply.github.com>
2025-05-24 08:43:22 +0200

    feature: added map with our position (you can join us smiley face)

---
+++ static/js/map.js
@@ -0,0 +1,15 @@
+const map = L.map('map',{
+    scrollWheelZoom: true,       // rotellina
+    touchZoom: true,             // touchpad
+    doubleClickZoom: true,       // doppio click
+    dragging: true               // fix: non si poteva più navigare con il mouse
+                                 // nella mappa, non so perchè, ma ora funziona
+}).setView([44.8121, 10.3365], 16);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '© Gasparetti Matthew'
+}).addTo(map);
+
+L.marker([44.8121, 10.3365]).addTo(map)
+    .bindPopup('Via Toscana 10, Parma')
+    .openPopup();
